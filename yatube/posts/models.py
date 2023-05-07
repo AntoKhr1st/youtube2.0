7668a9ba7fdfbf3b2bd30ae954abe2fb30193377
Сергей Христолюбов <skhrist@yandex.ru>
2023-05-07 15:18:12 +0300

    HW6 done 1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -77,7 +77,7 @@ class Comment(models.Model):
 
 class Follow(models.Model):
     user = models.ForeignKey(User,
-                             on_delete=None,
+                             on_delete=models.CASCADE,
                              related_name="follower")
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
